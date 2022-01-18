911a67b19c57f36680613c73354dd03a717050ff
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2022-01-18 22:16:39 +0600

    New entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
-chown postgres.postgres /var/lib/postgresql/9.5/main/
+chown postgres:postgres -R /var/lib/postgresql/9.5/main/
+chmod 700 -R /var/lib/postgresql/9.5/main
 
 runuser -l postgres -c "/usr/lib/postgresql/9.5/bin/postgres -c config-file=/etc/postgresql.conf"
